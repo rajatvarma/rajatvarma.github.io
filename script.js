c39c80f06e7a7e5c6d9bd3e29790237ba89d134e
Rajat Varma <rajatvarma08@gmail.com>
2020-01-14 23:15:17 +0530

    fixing stuff

--- script.js
+++ script.js
@@ -8,12 +8,14 @@ function switchTheme() {
     var aboutArea = document.getElementsByClassName("about")
     var themeSwitchButton = document.getElementById("themeSwitcherButton");
     if (isDark == true) {
+        document.documentElement.style.setProperty('--primary',  '#56C3F2');
         body[0].style.backgroundColor = "var(--dark-bg)";
         aboutArea[0].style.color = "var(--primary)";
         themeSwitchButton.innerHTML = "<img src='imgs/moon.svg'>";       
     }
 
     else if (isDark == false) {
+        document.documentElement.style.setProperty('--primary',  '#3AB9EF');
         body[0].style.backgroundColor = "var(--light-bg)";
         aboutArea[0].style.color = "var(--primary)";
         themeSwitchButton.innerHTML = "<img src='imgs/sun.svg'>";
@@ -32,3 +34,4 @@ function linksPosition(){
 }
 
 switchTheme();
+
